7e6ba02688752374be9bc4b4512acb85f3d41c6f
Oxida007 <123311208+Oxida007@users.noreply.github.com>
2023-01-22 21:44:11 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import random #Coded by Oxida
+
+#Password
+if password == "test":
+  pass
+else:
+    exit()
+
+num1 = int(input("First num : "))
+num2 = int(input("Sec num : "))
+
+result = num1 * num2;
+
+print("Result's : ", (result))
